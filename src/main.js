a89b00e33a657373591e270732a4455c23c6b3b2
Dan Le <dhl1337@gmail.com>
2017-01-29 15:25:09 -0700

    add links

--- src/main.js
+++ src/main.js
@@ -1,41 +1,9 @@
-$ = jQuery = require('jquery'); // define jquery globally.
+'use strict';
 
 var React = require('react');
-var HomePage = require('./components/HomePage');
-var AboutPage = require('./components/about/AboutPage');
-var Header = require('./components/common/Header');
-var AuthorPage = require('./components/authors/AuthorPage');
+var Router = require('react-router');
+var routes = require('./routes');
 
-var App = React.createClass({
-    render: function () {
-        var Child;
-
-        switch (this.props.route) {
-            case 'about':
-                Child = AboutPage;
-                break;
-            case 'authors':
-                Child = AuthorPage;
-                break;
-            default:
-                Child = HomePage;
-        }
-
-        return (
-            <div>
-                <Header/>
-                <Child/>
-            </div>
-        );
-    }
+Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
 });
-
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route}/>, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
-
-//React.render(<HomePage/>, document.getElementById('app'));
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@
 
 var React = require('react');
 var Router = require('react-router');
-var Default = Router.DefaultRoute;
+var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 
+
 var routes = (
-    <Route name='app' path='/' handler={require('./components/app')}>
+    <Route name="app" path="/" handler={require('./components/App')}>
         <DefaultRoute handler={require('./components/HomePage')} />
-        <Route name='authors' handler={require('./components/AuthorPage')} />
+        <Route name='authors' handler={require('./components/authors/AuthorPage')} />
         <Route name='about' handler={require('./components/about/AboutPage')} />
     </Route>
 )
