b6f8949172fe338ce5ed92ce6e9eb638a19a7ae1
Dan Le <dhl1337@gmail.com>
2017-01-30 12:18:30 -0700

    add redirect after saving data

--- src/components/authors/ManageAuthorPage.js
+++ src/components/authors/ManageAuthorPage.js
@@ -1,10 +1,16 @@
 'use strict';
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./AuthorForm');
 var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function () {
         return {
             author: {id: '', firstName: '', lastName: ''}
@@ -21,6 +27,7 @@ var ManageAuthorPage = React.createClass({
     saveAuthor: function (event) {
         event.preventDefault();
         AuthorApi.saveAuthor(this.state.author);
+        this.transitionTo('authors');
     },
 
     render: function () {
