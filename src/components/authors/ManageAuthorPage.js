8970c1c08bd51179af991d07b8e86f25e18cc39e
Dan Le <dhl1337@gmail.com>
2017-01-30 12:48:51 -0700

    add validation

--- src/components/authors/ManageAuthorPage.js
+++ src/components/authors/ManageAuthorPage.js
@@ -14,19 +14,43 @@ var ManageAuthorPage = React.createClass({
 
     getInitialState: function () {
         return {
-            author: {id: '', firstName: '', lastName: ''}
+            author: {id: '', firstName: '', lastName: ''},
+            errors: {}
         };
     },
 
+    authorFormIsValid: function () {
+        var formIsValid = true;
+        this.state.errors = {}; // clear any previous errors
+
+        if (this.state.author.firstName.length < 3) {
+            this.state.errors.firstName = 'First name must be at least 3 characters.';
+            formIsValid = false;
+        }
+
+        if (this.state.author.lastName.length < 3) {
+            this.state.errors.lastName = 'Last name must be at least 3 characters.';
+            formIsValid = false;
+        }
+
+        this.setState({errors: this.state.errors});
+        return formIsValid;
+    },
+
     setAuthorState: function (event) {
+
         var field = event.target.name;
         var value = event.target.value;
+
         this.state.author[field] = value;
         return this.setState({author: this.state.author});
     },
 
     saveAuthor: function (event) {
         event.preventDefault();
+        if (!this.authorFormIsValid()) {
+            return;
+        }
         AuthorApi.saveAuthor(this.state.author);
         toastr.success('Author saved.');
         this.transitionTo('authors');
@@ -37,7 +61,8 @@ var ManageAuthorPage = React.createClass({
             <AuthorForm
                 author={this.state.author}
                 onChange={this.setAuthorState}
-                onSave={this.saveAuthor}/>
+                onSave={this.saveAuthor}
+                errors={this.state.errors} />
         );
     }
 });
